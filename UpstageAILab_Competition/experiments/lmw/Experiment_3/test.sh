4a6a5b216b9aef9dac52cbfbec270c026aecaf22
LMWoo <leemw513@naver.com>
2025-05-15 07:53:26 +0000

    feat: Add Experiment3
    Feature Engineering, Experiment bash

---
+++ UpstageAILab_Competition/experiments/lmw/Experiment_3/test.sh
@@ -0,0 +1,7 @@
+python main.py \
+    --data=lmw.Data_3_dataprocess.Data_3_Preprocess \
+    --model=lmw.lightGBM_model.LightGBMModel \
+    --data_root_path=../data \
+    --model_name=Experiment_3 \
+    --test_result_file=Experiment_3 \
+    --mode=test
